53762ee3be2cade53ee7754211ad160f039f9a35
leftygamer <dashes_chump0h@icloud.com>
2022-03-18 07:20:06 -0600

    Updated '/serverproperties' SendMessage methods

--- GameServer/commands/admincommands/serverproperties.cs
+++ GameServer/commands/admincommands/serverproperties.cs
@@ -16,6 +16,7 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
+
 using System;
 using DOL.GS.PacketHandler;
 using DOL.Language;
@@ -23,21 +24,34 @@ using DOL.Language;
 namespace DOL.GS.Commands
 {
 	[CmdAttribute(
+		// Enter '/serverproperties' to list all associated subcommands
 		"&serverproperties",
-		ePrivLevel.Admin,
+		// Message: '/serverproperties' - Refreshes the live cache with any changes from the 'serverproperty' table. 
+		"AdminCommands.ServerProp.CmdList.Description",
+		// Message: <----- '/{0}' Command {1}----->
+		"AllCommands.Header.General.Commands",
+		ePrivLevel.Admin, // Required minimum privilege level to use the command
+		// Message: Refreshes the live cache with any changes from the 'serverproperty' table.
 		"AdminCommands.ServerProperties.Description",
-		"AdminCommands.ServerProperties.Usage")]
+		// Syntax: /serverproperties
+		"AdminCommands.ServerProp.Syntax.ServerProp",
+		// Message: Refreshes the server's properties.
+		"AdminCommands.ServerProp.Usage.ServerProp")]
 	public class ServerPropertiesCommand : AbstractCommandHandler, ICommandHandler
 	{
 		public void OnCommand(GameClient client, string[] args)
 		{
+			// Dated code for people still using XML setups instead of DBs
 			if (GameServer.Instance.Configuration.DBType == DOL.Database.Connection.ConnectionType.DATABASE_XML)
 			{
-				DisplayMessage(client, LanguageMgr.GetTranslation(client.Account.Language, "AdminCommands.ServerProperties.DataBaseXML"));
+				// Message: XML is cached sorry, you cannot refresh server properties unless using MySQL!
+				ChatUtil.SendTypeMessage("error", client, "AdminCommands.ServerProp.XMLOld.Sorry", null);
 				return;
 			}
+			
 			ServerProperties.Properties.Refresh();
-			DisplayMessage(client, LanguageMgr.GetTranslation(client.Account.Language, "AdminCommands.ServerProperties.PropertiesRefreshed"));
+			// Message: Atlas' server properties have been refreshed!
+			ChatUtil.SendTypeMessage("important", client, "AdminCommands.ServerProp.Msg.PropsRefreshed", null);
 		}
 	}
 }
