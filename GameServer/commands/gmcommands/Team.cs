6813351871a3dc55775091502035917480adf501
leftygamer <dashes_chump0h@icloud.com>
2022-03-22 09:36:21 -0600

    Standardized messages for '/team' command

--- GameServer/commands/gmcommands/Team.cs
+++ GameServer/commands/gmcommands/Team.cs
@@ -17,31 +17,6 @@
  *
  */
 
-/* <--- SendMessage Standardization --->
-*  All messages now use translation IDs to both
-*  centralize their location and standardize the method
-*  of message calls used throughout this project. All messages affected
-*  are in English. Other languages are not yet supported.
-* 
-*  To  find a message at its source location, either use
-*  the message body contained in the comment above the return
-*  (e.g., // Message: This is a message.) or the
-*  translation ID (e.g., "AdminCommands.Account.Description").
-* 
-*  To perform message changes, take note of your server settings.
-*  If the `serverproperty` table setting `use_dblanguage`
-*  is set to `True`, you must make your changes from the
-*  `languagesystem` DB table.
-* 
-*  If the `serverproperty` table setting
-*  `update_existing_db_system_sentences_from_files` is set to `True`,
-*  perform changes to messages using the .txt files locate in "GameServer >
-*  language > EN".
-*
-*  OPTIONAL: After changing a message, paste the new string
-*  into the comment above the affected method(s). This is
-*  done for ease of reference. */
-
 using System;
 using DOL.GS;
 using DOL.GS.PacketHandler;
@@ -49,18 +24,24 @@ using DOL.Database;
 
 namespace DOL.GS.Commands
 {
-	// See the comments above 'using' about SendMessage translation IDs
+	/// <summary>
+	/// Handles all user-based interaction for the '/team' command
+	/// </summary>
 	[CmdAttribute( 
 	// Enter '/team' to see command syntax messages
 	"&team",
 	new [] { "&te" },
-	// Message: <----- '/team' Commands (plvl 2) ----->
-	"GMCommands.Header.Command.Team",
+	// Message: '/team' or '/te' - Broadcasts a message to all Atlas server team members (i.e., plvl 2+).
+	"GMCommands.CmdList.Team.Description",
+	// Message: <----- '/{0}' Command {1}----->
+	"AllCommands.Header.General.Commands",
+	// Required minimum privilege level to use the command
    ePrivLevel.GM,
-	"Broadcasts a message to all Atlas server team members (i.e., plvl 2+).",
+	// Message: Broadcasts a message to all Atlas server team members (i.e., plvl 2+).
+	"GMCommands.Team.Description",
 	// Syntax: '/team <message>' or '/te <message>'
 	"GMCommands.Team.Syntax.Team",
-	// Message: Broadcasts a message to all Atlas server team members (i.e., plvl 2+).
+	// Message: Broadcasts a message to the [TEAM] channel.
 	"GMCommands.Team.Usage.Team")]
 
 	public class TeamCommandHandler : AbstractCommandHandler, ICommandHandler
@@ -70,14 +51,11 @@ namespace DOL.GS.Commands
 			// Lists all '/team' command syntax
 			if (args.Length < 2)
 			{
-				// Message: <----- '/team' Commands (plvl 2) ----->
-				ChatUtil.SendHeaderMessage(client, "GMCommands.Header.Command.Team", null);
+				// Message: <----- '/{0}' Command {1}----->
 				// Message: Use the following syntax for this command:
-				ChatUtil.SendCommMessage(client, "AllCommands.Command.SyntaxDesc", null);
 				// Syntax: '/team <message>' or '/te <message>'
-				ChatUtil.SendSyntaxMessage(client, "GMCommands.Team.Syntax.Team", null);
-				// Message: Broadcasts a message to all Atlas server team members (i.e., plvl 2+).
-				ChatUtil.SendCommMessage(client, "GMCommands.Team.Usage.Team", null);	
+				// Message: Broadcasts a message to the [TEAM] channel.
+				DisplayHeadSyntax(client, "team", "", "", 2, false, "GMCommands.Team.Syntax.Team", "GMCommands.Team.Usage.Team");
 				return;
 			}
 
@@ -90,7 +68,7 @@ namespace DOL.GS.Commands
 				if (player.Account.PrivLevel > 1)
 				{
 					// Message: [TEAM] {0}: {1}
-					ChatUtil.SendTeamMessage(player, "Social.ReceiveMessage.Staff.Channel", client.Player.Name, message);
+					ChatUtil.SendTypeMessage("team", player, "Social.ReceiveMessage.Staff.Channel", client.Player.Name, message);
 				}
 			}
 		}
