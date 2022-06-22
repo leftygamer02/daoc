fd7fb19b19f713eb9b5cc556f6173d64b0d0c875
clait <stefanomus@fastmail.com>
2022-06-22 13:34:46 +0100

    GetFromID

--- GameServer/gameutils/XPItemUtils.cs
+++ GameServer/gameutils/XPItemUtils.cs
@@ -8,9 +8,17 @@ public class XPItemUtils
     {
         var xpItems = DOLDB<XPItem>.SelectObjects(DB.Column("MinLevel").IsLessThan(player.Level).And(DB.Column("MaxLevel").IsGreatherThan(player.Level)).And(DB.Column("Realm").IsEqualTo(player.Realm)));
         
-        var random = Util.Random(0, xpItems.Count);
+        if (xpItems.Count == 0)
+            return null;
+        
+        var random = Util.Random(0, xpItems.Count - 1);
         var xpitem = xpItems[random];
 
         return xpitem;
+    }
+    public static XPItem GetFromID(string id)
+    {
+        var xpItem = DOLDB<XPItem>.SelectObject(DB.Column("XPItemID").IsEqualTo(id));
+        return xpItem;
     }
 }
