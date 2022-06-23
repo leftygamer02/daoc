0005f58bf4a36efee48a611d90e2e3a4bdd7b9b7
clait <stefanomus@fastmail.com>
2022-06-23 15:03:42 +0100

    just sad

--- GameServer/spells/XPBuff.cs
+++ GameServer/spells/XPBuff.cs
@@ -7,29 +7,12 @@ namespace DOL.GS.Spells
 	/// </summary>
 	///
 	[SpellHandler("XPBuff")]
-	public class XPBuff : SpellHandler
+	public class XPBuff : PropertyChangingSpell
 	{
 		public XPBuff(GameLiving caster, Spell spell, SpellLine spellLine) : base(caster, spell, spellLine) { }
 
 
-		public override void OnEffectStart(GameSpellEffect effect)
-		{
-			if (effect.Owner is GamePlayer player)
-			{
-				player.BaseBuffBonusCategory[(int)eProperty.XpPoints] += (int)Spell.Value;
-			}
-
-			base.OnEffectStart(effect);
-		}
-
-		public override int OnEffectExpires(GameSpellEffect effect, bool noMessages)
-		{
-			if (effect.Owner is GamePlayer player)
-			{
-				player.BaseBuffBonusCategory[(int)eProperty.XpPoints] -= (int)Spell.Value;
-			}
-			
-			return base.OnEffectExpires(effect, noMessages);
-		}
+		public override eBuffBonusCategory BonusCategory1 { get { return eBuffBonusCategory.BaseBuff; } }
+		public override eProperty Property1 { get { return eProperty.XpPoints; } }
 	}
 }
