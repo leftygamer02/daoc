6df50142707821c74bdf5dda0d1f6bd4d135286d
Meeks Meeks <blurubberduck87@gmail.com>
2022-06-25 04:00:15 -0700

    renamed command. killing all mobs within max ushort range to test ProcessDeath

---
+++ GameServer/commands/admincommands/TestServiceCommand.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Reflection;
+using DOL.GS.Commands;
+using DOL.GS.PacketHandler;
+using log4net;
+
+namespace DOL.GS.Commandss {
+    [Cmd("&TestService",
+		ePrivLevel.Admin,
+		"Test a service. Used for debugging performance",
+		"/testservice mob|object|specs|spells|teleports"
+		)]
+	public class TestServiceCommand : AbstractCommandHandler, ICommandHandler {
+		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+		public void OnCommand(GameClient client, string[] args) {
+			if (args.Length < 2) {
+				DisplaySyntax(client);
+				return;
+			}
+			GamePlayer player = client.Player;
+
+			switch(args[1]) {
+				case "mob":
+					_testMobService(player);
+					break;
+				default:
+					break;
+			}			
+		}
+
+		private void _testMobService(GamePlayer player) {
+			int count = 0;
+			foreach (GameNPC living in player.GetNPCsInRadius(ushort.MaxValue, true)) {
+				if (!living.IsAlive || living.ObjectState != GameObject.eObjectState.Active) {
+					continue;
+				}
+				kill(player.Client, living);
+				count++;
+			}
+			player.Out.SendMessage($"{count} mobs successfully killed", eChatType.CT_System, eChatLoc.CL_ChatWindow);
+		}
+		private void kill(GameClient client, GameNPC targetMob) {
+			try {
+				lock (targetMob.XPGainers.SyncRoot) {
+					targetMob.attackComponent.AddAttacker(client.Player);
+					targetMob.AddXPGainer(client.Player, targetMob.Health);
+					targetMob.Die(client.Player);
+					client.Out.SendMessage("Mob '" + targetMob.Name + "' killed", eChatType.CT_System, eChatLoc.CL_SystemWindow);
+				}
+			} catch (Exception e) {
+				client.Out.SendMessage(e.ToString(), eChatType.CT_System, eChatLoc.CL_SystemWindow);
+			}
+		}
+	}
+}
