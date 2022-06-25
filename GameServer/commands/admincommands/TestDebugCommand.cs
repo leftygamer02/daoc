88cc9c68938d370e5c2a54a2a5d57fec592e9cc7
Meeks Meeks <blurubberduck87@gmail.com>
2022-06-25 03:03:49 -0700

    added cmd

---
+++ GameServer/commands/admincommands/TestDebugCommand.cs
@@ -0,0 +1,22 @@
+﻿using System.Reflection;
+using DOL.GS.Commands;
+using log4net;
+
+namespace DOL.GS.Commandss {
+    [Cmd("&TestService",
+		ePrivLevel.Admin,
+		"Sends",
+		"/testservice mob|object|specs|spells|teleports"
+		)]
+	public class TestService : AbstractCommandHandler, ICommandHandler {
+		private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+		public void OnCommand(GameClient client, string[] args) {
+			if (args.Length < 2) {
+				DisplaySyntax(client);
+				return;
+			}
+			GamePlayer target = client.Player;
+
+		}
+	}
+}
