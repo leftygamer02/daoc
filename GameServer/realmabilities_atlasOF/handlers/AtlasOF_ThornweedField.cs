5fd6b79c66f450ac2dab7401a51335225d3bcf8b
Anthony Knight <midgetlord123@hotmail.com>
2022-10-12 12:43:50 -0700

    Lower cost and re-use time of Thornweed Field

--- GameServer/realmabilities_atlasOF/handlers/AtlasOF_ThornweedField.cs
+++ GameServer/realmabilities_atlasOF/handlers/AtlasOF_ThornweedField.cs
@@ -9,7 +9,7 @@ namespace DOL.GS.RealmAbilities
         public AtlasOF_ThornweedField(DBAbility dba, int level) : base(dba, level) { }
 
         public override int MaxLevel { get { return 1; } }
-        public override int CostForUpgrade(int level) { return 14; }
-        public override int GetReUseDelay(int level) { return 1800; } // 30 mins
+        public override int CostForUpgrade(int level) { return 10; }
+        public override int GetReUseDelay(int level) { return 900; } // 15 mins
     }
 }
