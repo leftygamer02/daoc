579eb466c3c92c528d955f18e6666456e5af0168
Anthony Knight <midgetlord123@hotmail.com>
2022-10-12 12:39:33 -0700

    Lower cost of Corporeal Disintegration

--- GameServer/realmabilities_atlasOF/handlers/AtlasOF_CorporealDisintegration.cs
+++ GameServer/realmabilities_atlasOF/handlers/AtlasOF_CorporealDisintegration.cs
@@ -27,7 +27,7 @@ namespace DOL.GS.RealmAbilities
         private SpellLine m_spellline;
 
         public override int MaxLevel { get { return 1; } }
-		public override int CostForUpgrade(int level) { return 14; }
+		public override int CostForUpgrade(int level) { return 10; }
 		public override int GetReUseDelay(int level) { return 900; } // 15 mins
 
         private void CreateSpell(GamePlayer caster)
