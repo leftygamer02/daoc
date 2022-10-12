cd620af9d5fbcdf88b99580f429f7cb21f3041e9
Anthony Knight <midgetlord123@hotmail.com>
2022-10-12 12:43:12 -0700

    Lower re-use time of Soldier's Barricade

--- GameServer/realmabilities_atlasOF/handlers/AtlasOF_SoldiersBarricade.cs
+++ GameServer/realmabilities_atlasOF/handlers/AtlasOF_SoldiersBarricade.cs
@@ -10,7 +10,7 @@ namespace DOL.GS.RealmAbilities
 
         public override int MaxLevel { get { return 1; } }
         public override int CostForUpgrade(int level) { return 10; }
-        public override int GetReUseDelay(int level) { return 1800; } // 30 mins
+        public override int GetReUseDelay(int level) { return 900; } // 15 mins
         protected override int GetArmorFactorAmount() { return 330; }
 
         public override void AddEffectsInfo(IList<string> list)
