f52397f5a32932b01f4c9059c7f03e4ef3fefba5
leftygamer <leftygamer02@pm.me>
2023-03-13 10:01:14 -0600

    Disabled '/gc unsummon' command

--- GameServer/commands/playercommands/guild.cs
+++ GameServer/commands/playercommands/guild.cs
@@ -1391,7 +1391,13 @@ namespace DOL.GS.Commands
 							return;
 						}
 					#endregion Buff
-						#region Unsummon
+					#region Unsummon (Disabled)
+					// --------------------------------------------------------------------------------
+					// SUMMON
+					// '/gc unsummon'
+					// Removes the guild banner if you have one active
+					// --------------------------------------------------------------------------------
+					/* Disabled because it is not used for Eclipse
 					case "unsummon":
 						{
 							if (client.Player.Guild == null)
@@ -1429,8 +1435,9 @@ namespace DOL.GS.Commands
 								client.Out.SendMessage("You aren't carrying a banner!", eChatType.CT_System, eChatLoc.CL_SystemWindow);
 							}
 							break;
-						}
-						#endregion
+						} 
+					*/
+					#endregion Unsummon (Disabled)
 						#region Ranks
 					case "ranks":
 						{
